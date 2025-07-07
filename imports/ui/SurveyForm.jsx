7d9f996ed569598028ba50f1eaa96782bea75dd5
Bosu <badebosubabu@gmail.com>
2025-07-07 11:56:22 -0400

    feat: add SurveyForm component to render dynamic SurveyJS form

---
+++ imports/ui/SurveyForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Survey } from 'survey-react-ui';
+import 'survey-core/defaultV2.min.css';
+
+const SurveyForm = ({ surveyModel }) => {
+  if (!surveyModel) {
+    return <p>No form to display yet. Capture an ID first!</p>;
+  }
+
+  return (
+    <div>
+      <h2>Visitor Registration Form</h2>
+      <Survey model={surveyModel} />
+    </div>
+  );
+};
+
+export default SurveyForm;
