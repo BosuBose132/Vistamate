999a0ecf30c07938b62b979a8febe94c08cd1269
Bosu <badebosubabu@gmail.com>
2025-07-18 20:02:09 -0400

    feat: add QR code for the vCard

--- imports/ui/SurveyForm.jsx
+++ imports/ui/SurveyForm.jsx
@@ -1,11 +1,16 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import SurveyForm from './SurveyForm';
 import { Survey } from 'survey-react-ui';
 import 'survey-core/survey-core.css';
 import { generateVCard } from '../../utils/vcard';
+import { QRCode } from 'qrcode.react';
+
 
 const SurveyForm = ({ surveyModel, onReturnHome }) => {
   const [isCompleted, setIsCompleted] = useState(false);
   const [submittedData, setSubmittedData] = useState(null);
+  const [visitorData, setVisitorData] = useState(null);
+
 
   useEffect(() => {
     if (surveyModel?.isCompleted) {
@@ -32,7 +37,11 @@ const SurveyForm = ({ surveyModel, onReturnHome }) => {
     return (
       <div style={{ textAlign: 'center', marginTop: '2rem' }}>
         <h2>Thank you for submitting the form!</h2>
-        <button className="btn btn-success mt-3" onClick={handleDownloadVCard}>
+        <div className="my-4">
+          <p>Scan to get contact details</p>
+          <QRCode value={generateVCard(visitorData)} size={160} />
+        </div>
+        <button className="btn btn-success gap-3 mt-4" onClick={handleDownloadVCard}>
           Download Contact Card
         </button>
         <button
@@ -44,12 +53,13 @@ const SurveyForm = ({ surveyModel, onReturnHome }) => {
       </div>
     );
   }
-   return (
+  return (
     <div>
       <h2></h2>
       <Survey model={surveyModel}
         onComplete={(sender) => {
           const data = sender.data;
+          setVisitorData(data);
           setSubmittedData(data);
           setIsCompleted(true);
         }} />
