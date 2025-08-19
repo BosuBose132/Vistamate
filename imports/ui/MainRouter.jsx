320d5df308f7789c1f1b335950e68602a472de5e
Bosu <badebosubabu@gmail.com>
2025-08-19 15:21:39 -0400

    add imports and routes by minimal route wiring

--- imports/ui/MainRouter.jsx
+++ imports/ui/MainRouter.jsx
@@ -9,6 +9,10 @@ import App from './pages/App';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
 import ThankYou from './pages/ThankYou';
+import StationKiosk from './station/StationKiosk';
+import StationBuilder from './admin/stations/StationBuilder';
+import SurveyManager from './admin/surveys/SurveyManager';
+import StationDashboard from './admin/dashboard/StationDashboard';
 
 const ProtectedRoute = ({ children }) => {
     const user = useTracker(() => Meteor.user());
@@ -20,9 +24,25 @@ const MainRouter = () => (
         <Routes>
             <Route path="/" element={<WelcomePage />} />
             <Route path="/checkin" element={<App />} />
+            <Route path="/s/:token" element={<StationKiosk />} />
             <Route path="/admin" element={
                 <ProtectedRoute>
                     <Admin />
+                </ProtectedRoute>
+            } />
+            <Route path="/admin/stations" element={
+                <ProtectedRoute>
+                    <StationBuilder />
+                </ProtectedRoute>
+            } />
+            <Route path="/admin/surveys" element={
+                <ProtectedRoute>
+                    <SurveyManager />
+                </ProtectedRoute>
+            } />
+            <Route path="/admin/checkins" element={
+                <ProtectedRoute>
+                    <StationDashboard />
                 </ProtectedRoute>
             } />
             <Route path="/login" element={<Login />} />
